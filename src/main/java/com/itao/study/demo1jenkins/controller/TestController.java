820ae2ccadd166c4e0138083998674c7032d8d07
taojun <taojun@tuzhanai.com>
2020-06-09 10:57:54 +0800

    11

---
+++ src/main/java/com/itao/study/demo1jenkins/controller/TestController.java
@@ -0,0 +1,27 @@
+package com.itao.study.demo1jenkins.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+/**
+ * @Auther: taojun
+ * @Date: 2020/6/9 10:44
+ * @Description:
+ */
+@RestController
+@RequestMapping("/test")
+@Slf4j
+public class TestController {
+
+    Random random = new Random();
+
+    @RequestMapping("/test1")
+    public String test1(){
+        int num = random.nextInt(99999);
+        log.info("test.test1 num = {}",num);
+        return num+"";
+    }
+}
---
+++ src/test/java/com/itao/study/demo1jenkins/Demo1JenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itao.study.demo1jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1JenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
